23b1cc4ddd83019377ef372910ec92d9272b173d
Dave Carroll <powershell@anovelidea.org>
2020-10-13 12:58:08 -0500

    clodse #15

--- Twitter/public/Get-TwitterList.ps1
+++ Twitter/public/Get-TwitterList.ps1
@@ -1,98 +1,51 @@
 function Get-TwitterList {
-    [CmdletBinding(DefaultParameterSetName='List')]
+    [CmdletBinding(DefaultParameterSetName='ListScreenName')]
     param(
-        [Parameter(ParameterSetName='List')]
+        [Parameter(Mandatory,ParameterSetName='ListScreenName')]
+        [ValidateNotNullOrEmpty()]
         [string]$ScreenName,
-        [Parameter(ParameterSetName='List')]
+        [Parameter(Mandatory,ParameterSetName='ListUserId')]
+        [ValidateNotNullOrEmpty()]
         [string]$UserId,
-        [Parameter(ParameterSetName='List')]
+        [Parameter(ParameterSetName='ListScreenName')]
+        [Parameter(ParameterSetName='ListUserId')]
         [switch]$OwnedListFirst,
 
         [Parameter(Mandatory,ParameterSetName='ShowId')]
+        [ValidateNotNullOrEmpty()]
         [string]$ListId,
 
-        [Parameter(Mandatory,ParameterSetName='ShowSlug')]
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerScreenName')]
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwernId')]
+        [ValidateNotNullOrEmpty()]
         [string]$Slug,
-        [Parameter(ParameterSetName='ShowSlug')]
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerScreenName')]
+        [ValidateNotNullOrEmpty()]
         [string]$OwnerScreenName,
-        [Parameter(ParameterSetName='ShowSlug')]
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwernId')]
+        [ValidateNotNullOrEmpty()]
         [long]$OwnerId
     )
 
-    switch($PSCmdlet.ParameterSetName) {
-        'List' {
-            $Query = [hashtable]::new()
-            if ($ScreenName) {
-                $Query.Add('screen_name',$ScreenName)
-            }
-            if ($UserId) {
-                $Query.Add('user_id',$UserId)
-            }
-            if ($PSBoundParameters.ContainsKey('OwnedListFirst')) {
-                $Query.Add('reverse','true')
-            }
+    if ($PSCmdlet.ParameterSetName -match 'List') {
 
-            $OAuthParameters = [OAuthParameters]::new(
-                'Get',
-                'https://api.twitter.com/1.1/lists/list.json',
-                $Query
-            )
+        $Query = New-TwitterQuery -ApiParameters $PSBoundParameters
+        $OAuthParameters = [OAuthParameters]::new(
+            'Get',
+            'https://api.twitter.com/1.1/lists/list.json',
+            $Query
+        )
+        Invoke-TwitterRequest -OAuthParameters $OAuthParameters
 
-            Invoke-TwitterRequest -OAuthParameters $OAuthParameters
-        }
-        'ShowId' {
-            $Query = [hashtable]::new()
-            $Query.Add('list_id',$ListId)
-            $OAuthParameters = [OAuthParameters]::new(
-                'Get',
-                'https://api.twitter.com/1.1/lists/show.json',
-                $Query
-            )
+    } else {
 
-            Invoke-TwitterRequest -OAuthParameters $OAuthParameters
-        }
-        'ShowSlug' {
-            $SlugOwnerMessage = 'You must provide the Slug''s owner via the OwnerScreenName or OwnerId parameters'
-            $SlugOwnerRequired = 'You must provide the Slug''s owner.'
+        $Query = New-TwitterQuery -ApiParameters $PSBoundParameters
+        $OAuthParameters = [OAuthParameters]::new(
+            'Get',
+            'https://api.twitter.com/1.1/lists/show.json',
+            $Query
+        )
+        Invoke-TwitterRequest -OAuthParameters $OAuthParameters
 
-            $ValidationErrorRecord = @{
-                Message = $null
-                Parameter = 'Owner Parameters'
-                ErrorId = 'OwnerScreenNameOrOwnerId'
-            }
-
-            if (-Not ($PSBoundParameters.ContainsKey('OwnerScreenName')) -and (-Not $PSBoundParameters.ContainsKey('OwnerId'))) {
-                $ValidationErrorRecord.Message = '{0}.' -f $SlugOwnerMessage
-            } elseif ($PSBoundParameters.ContainsKey('OwnerScreenName') -and $PSBoundParameters.ContainsKey('OwnerId')) {
-                $ValidationErrorRecord.Message = '{0}, but not both.' -f $SlugOwnerMessage
-            } else {
-                if ($PSBoundParameters.ContainsKey('OwnerScreenName') -and [string]::IsNullOrWhiteSpace($OwnerScreenName)) {
-                    $ValidationErrorRecord.Message = $SlugOwnerRequired
-                }
-                if ($PSBoundParameters.ContainsKey('OwnerId') -and [string]::IsNullOrWhiteSpace($OwnerId)) {
-                    $ValidationErrorRecord.Message = $SlugOwnerRequired
-                }
-            }
-            if ($null -ne $ValidationErrorRecord.Message) {
-                $PSCmdlet.ThrowTerminatingError((New-ValidationErrorRecord @ValidationErrorRecord))
-            }
-
-            $Query = [hashtable]::new()
-            $Query.Add('slug',$Slug)
-            if ($OwnerScreenName) {
-                $Query.Add('owner_screen_name',$OwnerScreenName)
-            }
-            if ($OwnerId) {
-                $Query.Add('owner_id',$OwnerId)
-            }
-
-            $OAuthParameters = [OAuthParameters]::new(
-                'Get',
-                'https://api.twitter.com/1.1/lists/show.json',
-                $Query
-            )
-
-            Invoke-TwitterRequest -OAuthParameters $OAuthParameters
-        }
     }
 }
