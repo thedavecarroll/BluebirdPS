d29f1b810f0c46136cba31b95fa463607c962f57
Dave Carroll <powershell@anovelidea.org>
2020-10-12 22:16:35 -0500

    fix #13

--- Twitter/public/Unpublish-TwitterDM.ps1
+++ Twitter/public/Unpublish-TwitterDM.ps1
@@ -3,13 +3,14 @@ function Unpublish-TwitterDM {
     param(
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
+        [Alias('Id')]
         [string]$DirectMessageId
     )
 
     $OAuthParameters = [OAuthParameters]::new(
         'DELETE',
         'https://api.twitter.com/1.1/direct_messages/events/destroy.json',
-        @{'id'='mymessage'}
+        @{'id'= $DirectMessageId }
     )
     Invoke-TwitterRequest -OAuthParameters $OAuthParameters
 }
