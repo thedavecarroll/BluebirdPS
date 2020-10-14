cd0ba1f0666861f7314f8915445dd7af14ddd3a1
Dave Carroll <powershell@anovelidea.org>
2020-10-13 21:38:04 -0500

    close #17

---
+++ Twitter/public/Get-TwitterListSubscriber.ps1
@@ -0,0 +1,33 @@
+function Get-TwitterListSubscriber {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ParameterSetName='ShowId')]
+        [ValidateNotNullOrEmpty()]
+        [string]$ListId,
+
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerScreenName')]
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerId')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Slug,
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerScreenName')]
+        [ValidateNotNullOrEmpty()]
+        [string]$OwnerScreenName,
+        [Parameter(Mandatory,ParameterSetName='ShowSlugOwnerId')]
+        [ValidateNotNullOrEmpty()]
+        [long]$OwnerId,
+
+        [ValidateRange(1,5000)]
+        [int]$ResultsPerPage = 20,
+        [switch]$SkipStatus,
+        [switch]$ExcludeEntities
+    )
+
+    $Query = New-TwitterQuery -ApiParameters $PSBoundParameters
+    $OAuthParameters = [OAuthParameters]::new(
+        'Get',
+        'https://api.twitter.com/1.1/lists/subscribers.json',
+        $Query
+    )
+    Invoke-TwitterCursorRequest -OAuthParameters $OAuthParameters -ReturnValue users
+
+}
