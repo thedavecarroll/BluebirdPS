ca66cf76c020790cdf4829ec6b2e1d9de22e44ba
Dave Carroll <powershell@anovelidea.org>
2020-11-22 12:05:34 -0600

    silence verbose for twitter calls, fix verbose statement

--- BluebirdPS/public/api/supporting/Export-TwitterResource.ps1
+++ BluebirdPS/public/api/supporting/Export-TwitterResource.ps1
@@ -7,25 +7,25 @@ function Export-TwitterResource {
         [switch]$Languages
     )
 
-    if (-Not (Test-TwitterAuthentication)) {
+    if (-Not (Test-TwitterAuthentication -Verbose:$false)) {
         'Unable to connect to Twitter. Please use Set-TwitterAuthentication to set the requried API keys and secrets. Use the -Persist switch to save the values to disk.' | Write-Error -ErrorAction Stop
         return
     }
 
     switch ($PSCmdlet.ParameterSetName) {
         'Configuration' {
-            $Resource = Get-TwitterConfiguration
+            $Resource = Get-TwitterConfiguration -Verbose:$false
             $ResourceSavePath = $ConfigurationSavePath
         }
         'Languages' {
-            $Resource = Get-TwitterLanguages
+            $Resource = Get-TwitterLanguages -Verbose:$false
             $ResourceSavePath = $LanguagesSavePath
         }
     }
 
     try {
         $Resource | ConvertTo-Json | Set-Content -Path $ResourceSavePath -Force
-        'Saved Twitter Languages to {0}' -f $ResourceSavePath | Write-Verbose
+        'Saved Twitter {0} to {1}' -f $PSCmdlet.ParameterSetName,$ResourceSavePath | Write-Verbose
     }
     catch {
         $PSCmdlet.ThrowTerminatingError($_)
