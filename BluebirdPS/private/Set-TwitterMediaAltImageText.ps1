8fb62c6e3267e4759b626d08963f506c54cfdeaf
Dave Carroll <powershell@anovelidea.org>
2020-11-21 19:42:36 -0600

    add private function for alt text

---
+++ BluebirdPS/private/Set-TwitterMediaAltImageText.ps1
@@ -0,0 +1,19 @@
+function Set-TwitterMediaAltImageText {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias('media_id')]
+        [string]$MediaId,
+
+        [ValidateLength(1,1000)]
+        [string]$AltImageText
+    )
+
+    $OAuthParameters = [OAuthParameters]::new(
+        'POST',
+        'https://upload.twitter.com/1.1/media/metadata/create.json'
+    )
+
+    $OAuthParameters.Body = ('{{"media_id":"{0}","alt_text":{{"text":"{1}"}}}}' -f $MediaId,$AltImageText)
+    Invoke-TwitterRequest -OAuthParameters $OAuthParameters
+}
