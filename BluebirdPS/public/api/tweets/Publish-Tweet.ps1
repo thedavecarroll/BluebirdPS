9ba72c3f19ef7ce5d201f74b079ade82b074886d
Dave Carroll <powershell@anovelidea.org>
2020-11-21 21:52:35 -0600

    remove unused params, correct typo, use param set

--- BluebirdPS/public/api/tweets/Publish-Tweet.ps1
+++ BluebirdPS/public/api/tweets/Publish-Tweet.ps1
@@ -1,12 +1,11 @@
 function Publish-Tweet {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Default')]
     param(
         [Parameter(Mandatory)]
         [string]$TweetText,
-        [long]$ReplyToTweeet,
-        [object[]]$MediaId,
-        [switch]$Sensitive,
-        [object]$Place
+        [Parameter(ParameterSetName='Reply')]
+        [long]$ReplyToTweet,
+        [object[]]$MediaId
     )
 
     # https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
@@ -19,10 +18,12 @@ function Publish-Tweet {
     $Query = [hashtable]::new()
     $Query.Add('status', [System.Uri]::EscapeDataString($TweetText))
 
-    $Query.Add('in_reply_to_status_id', $ReplyToTweet)
+    if ($PSCmdlet.ParameterSetName -eq 'Reply') {
+        $Query.Add('in_reply_to_status_id', $ReplyToTweet)
 
-    # this will use the tweet id to get the screen_name and append it to the @mentions until @mentions have reached the limit.
-    $Query.Add('auto_populate_reply_metadata', 'true')
+        # this will use the tweet id to get the screen_name and append it to the @mentions until @mentions have reached the limit.
+        $Query.Add('auto_populate_reply_metadata', 'true')
+    }
 
     if ($MediaId.Count -gt 0) {
         $Query.Add('media_ids', ($MediaId -join ','))
