d65e72357a5eb09b3172c2083423bede63bf835d
Dave Carroll <powershell@anovelidea.org>
2021-06-16 09:22:48 -0500

    Add close #74

--- BluebirdPS/public/api/authentication/Import-TwitterAuthentication.ps1
+++ BluebirdPS/public/api/authentication/Import-TwitterAuthentication.ps1
@@ -2,15 +2,32 @@ function Import-TwitterAuthentication {
     [CmdletBinding()]
     param()
 
-    'Checking Twitter credentials file.' | Write-Verbose
+    'Checking for Twitter authentication.' | Write-Verbose
 
-    if (Test-Path -Path $BluebirdPSConfiguration.CredentialsPath) {
+    $BluebirdPSAuthEnvPaths =
+        'env:BLUEBIRDPS_API_KEY',
+        'env:BLUEBIRDPS_API_SECRET',
+        'env:BLUEBIRDPS_ACCESS_TOKEN',
+        'env:BLUEBIRDPS_ACCESS_TOKEN_SECRET'
 
-        'Twitter credentials file found.' | Write-Verbose
+    $BluebirdPSAuthEnvPaths = 'env:BLUEBIRDPS_BEARER_TOKEN'
+
+    if ((Test-Path -Path $BluebirdPSAuthEnvPaths) -notcontains $false) {
+        'Importing Twitter authentication from environment variables.' | Write-Verbose
+
+        $OAuth['ApiKey'] = $env:BLUEBIRDPS_API_SECRET
+        $OAuth['ApiSecret'] = $env:BLUEBIRDPS_API_SECRET
+        $OAuth['AccessToken'] = $env:BLUEBIRDPS_ACCESS_TOKEN
+        $OAuth['AccessTokenSecret'] = $env:BLUEBIRDPS_ACCESS_TOKEN_SECRET
+
+        if (Test-Path -Path $BluebirdPSBearerTokenEnvironmentPath) {
+            $OAuth['BearerToken'] = $env:BLUEBIRDPS_BEARER_TOKEN
+        }
+
+    } elseif (Test-Path -Path $BluebirdPSConfiguration.CredentialsPath) {
 
         try {
-
-            'Attempting to import Twitter credentials file.' | Write-Verbose
+            'Importing Twitter authentication from credentials file.' | Write-Verbose
 
             # read the encrypted credentials file, decrypt, and convert from JSON to object
             $OAuthFromDisk = Get-Content -Path $BluebirdPSConfiguration.CredentialsPath | ConvertTo-SecureString -ErrorAction Stop |
@@ -25,25 +42,39 @@ function Import-TwitterAuthentication {
 
             # ensure that we have values for the four required keys
             if ($OAuthFromDisk.psobject.Properties.Where{$_.Name -ne 'BearerToken' -and $null -ne $_.Value}.count -eq 4) {
-
                 $OAuth['ApiKey'] = $OAuthFromDisk.ApiKey
                 $OAuth['ApiSecret'] = $OAuthFromDisk.ApiSecret
                 $OAuth['AccessToken'] = $OAuthFromDisk.AccessToken
                 $OAuth['AccessTokenSecret'] = $OAuthFromDisk.AccessTokenSecret
+            }
+
+            if ($null -ne $OAuthFromDisk.BearerToken) {
                 $OAuth['BearerToken'] = $OAuthFromDisk.BearerToken
+            }
+        }
+        catch {
+            'Unable to import Twitter authentication data from credentials file.',
+            'Please use the Set-TwitterAuthentication command to update the required API keys and secrets.' | Write-Warning
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    } else {
+        'Twitter authentication data was not discovered in environment variables or on disk in credentials file.',
+        'Please use the Set-TwitterAuthentication command to set the required API keys and secrets.',
+        'The authentication values will be encrypted and saved to disk.' | Write-Warning
+        return
+    }
 
-                'Twitter credentials file imported.' | Write-Verbose
+    try {
+        Invoke-TwitterVerifyCredentials | Out-Null
+    }
+    catch {
+        'Twitter authentication data appears to be invalid.','Please use the Set-TwitterAuthentication command to update your stored credentials.' | Write-Warning
+        $PSCmdlet.WriteError($_)
+    }
 
-                try {
-                    Invoke-TwitterVerifyCredentials | Out-Null
-                }
-                catch {
-                    'Authentication file appears to have invalid credentials. Please use the Set-TwitterAuthentication command to update your stored credentials.' | Write-Warning
-                    return
-                }
-                if ($null -eq $BluebirdPSConfiguration.AuthUserId) {
-                    Set-BluebirdPSAuthUser
-                }
+    if ($null -eq $BluebirdPSConfiguration.AuthUserId) {
+        Set-BluebirdPSAuthUser
+    }
 
     if ($null -eq $OAuth['BearerToken']) {
         'Bearer token not present in Twitter authentication data.','Attempting to retrieve current bearer token from Twitter.' | Write-Verbose
