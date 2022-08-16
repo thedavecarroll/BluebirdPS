5149bc1d687767a8543134e35f63c9f3866631eb
Dave Carroll <powershell@anovelidea.org>
2022-08-15 23:12:26 -0500

    Changed #104

--- BluebirdPS/public/api_v2/tweets/Get-Tweet.ps1
+++ BluebirdPS/public/api_v2/tweets/Get-Tweet.ps1
@@ -7,19 +7,21 @@ function Get-Tweet {
     )]
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory,Position=0,ParameterSetName='Tweet')]
+        [Parameter(Mandatory,Position=0)]
         [ValidateNotNullOrEmpty()]
         [ValidateCount(1,100)]
         [ValidatePattern('^[0-9]{1,19}$', ErrorMessage = "The tweet Id '{0}' is not valid.")]
         [string[]]$Id,
-        [Parameter(Mandatory,Position=0,ParameterSetName='Conversation')]
-        [ValidateNotNullOrEmpty()]
-        [ValidatePattern('^[0-9]{1,19}$', ErrorMessage = "The conversation tweet Id '{0}' is not valid.")]
-        [string]$ConversationId,
+
+        [switch]$IncludeExpansions,
+
+        [ValidateRange(10,100)]
+        [int]$MaxResultsPerPage=100,
+        [switch]$NoPagination,
+
         [switch]$NonPublicMetrics,
         [switch]$PromotedMetrics,
-        [switch]$OrganicMetrics,
-        [switch]$IncludeExpansions
+        [switch]$OrganicMetrics
     )
 
     $Request = [TwitterRequest]@{
@@ -30,26 +32,13 @@ function Get-Tweet {
         IncludeExpansions = $IncludeExpansions
     }
 
-    switch ($PSCmdlet.ParameterSetName) {
-        'Tweet' {
-            if ($Id.Count -gt 1) {
-                $Request.Query.Add('ids', ($Id -join ','))
-                $Request.Endpoint = 'https://api.twitter.com/2/tweets'
-            } else {
-                $Request.Endpoint = 'https://api.twitter.com/2/tweets/{0}' -f $Id
-            }
-        }
-        'Conversation' {
-            $OriginalTweet = Get-Tweet -Id $ConversationId
-            $OriginalTweet
-            if ($OriginalTweet.CreatedAt -lt (Get-Date).AddDays(-7)) {
-                'As searching by ConversationId is based on recent search from the Standard product track, you can only retreive a conversation that started within the last 7 days.' | Write-Warning
-                return
-            }
-            $Request.Query.Add('query',('conversation_id:{0}' -f $ConversationId))
-            $Request.Endpoint = 'https://api.twitter.com/2/tweets/search/recent'
-        }
+    if ($Id.Count -gt 1) {
+        $Request.Query.Add('ids',($Id -join ','))
+        $Request.Endpoint = 'https://api.twitter.com/2/tweets'
+    } else {
+        $Request.Endpoint = 'https://api.twitter.com/2/tweets/{0}' -f $Id
     }
 
     Invoke-TwitterRequest -RequestParameters $Request
+
 }
