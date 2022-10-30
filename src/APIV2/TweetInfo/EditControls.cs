162c41d12228dab0ac3fd86c07686ff359aba893
Dave Carroll <powershell@anovelidea.org>
2022-10-30 00:11:48 -0500

    close #148

---
+++ src/APIV2/TweetInfo/EditControls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BluebirdPS.APIV2.TweetInfo
+{
+    public class EditControls : TwitterObject
+    {
+        public bool IsEditEligible { get; set; }
+        public DateTime EditableUntil { get; set; } 
+        public long EditsRemaining { get; set; } 
+
+        public EditControls() { }
+        public EditControls(dynamic input)
+        {
+            OriginalObject = input;
+
+            IsEditEligible = input.is_edit_eligible;
+            EditableUntil = input.editable_until;
+            EditsRemaining = input.edits_remaining;
+        }
+
+        public override string ToString()
+        {
+            return $"IsEditEligibile: {IsEditEligible}, EditableUntil: {EditableUntil}, EditsRemaining: {EditsRemaining}";
+        }
+    }
+}
