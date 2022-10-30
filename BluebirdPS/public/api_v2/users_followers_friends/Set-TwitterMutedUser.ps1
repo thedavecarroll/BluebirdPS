3663b48942fc223d0e322a4572afcb76ac90786c
Dave Carroll <powershell@anovelidea.org>
2022-10-29 23:39:22 -0500

    close #135

--- BluebirdPS/public/api_v2/users_followers_friends/Set-TwitterMutedUser.ps1
+++ BluebirdPS/public/api_v2/users_followers_friends/Set-TwitterMutedUser.ps1
@@ -21,8 +21,12 @@ function Set-TwitterMutedUser {
         $Request.Endpoint = 'https://api.twitter.com/2/users/{0}/muting/{1}' -f $BluebirdPSConfiguration.AuthUserId,$User.Id
     }
 
-    $SetTwitterMutedUser = Invoke-TwitterRequest -RequestParameters $Request
-    $Muting = $SetTwitterMutedUser ? 'muting' : 'not muting'
-    'You are {0} user {1}' -f $Muting,$User.ToString()
-
+    try {
+        $SetTwitterMutedUser = Invoke-TwitterRequest -RequestParameters $Request
+        $Muting = $SetTwitterMutedUser ? 'muting' : 'not muting'
+        'You are {0} user {1}' -f $Muting,$User.ToString()
+    }
+    catch {
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
 }
