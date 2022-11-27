a8698c9aefe5f21b9a6c0631f1ea26324d732036
Dave Carroll <powershell@anovelidea.org>
2022-11-27 09:20:48 -0600

    close #198

--- BluebirdPS/public/helpers/Find-TwitterMastodonLinks.ps1
+++ BluebirdPS/public/helpers/Find-TwitterMastodonLinks.ps1
@@ -4,6 +4,7 @@ function Find-TwitterMastodonLinks {
     param(
         [Parameter(Mandatory,ValueFromPipeline)]
         [BluebirdPS.TwitterObject[]]$TwitterObject,
+        [ValidateNotNullOrEmpty()]
         [Alias('IgnoreUrl')]
         [string[]]$IgnoreDomain
     )
@@ -13,6 +14,11 @@ function Find-TwitterMastodonLinks {
         $MastodonUsers = [System.Collections.Generic.List[TwitterMastodonReference]]::new()
         $TwitterUsers =  [System.Collections.Generic.List[User]]::new()
         $Tweets =  [System.Collections.Generic.List[Tweet]]::new()
+        if ($PSBoundParameters.ContainsKey('IgnoreDomain')) {
+            $IgnoreDomainRegex = $IgnoreDomain -join '|'
+        } else {
+            $IgnoreDomainRegex = $false
+        }
     }
     process {
         foreach ($Object in $TwitterObject) {
@@ -27,24 +33,34 @@ function Find-TwitterMastodonLinks {
     end {
         foreach ($User in $TwitterUsers) {
             if ($User.Name -match  $MastodonUserAccountRegex) {
+                if ($IgnoreDomainRegex) {
+                    if ($Matches['MastodonInstance'] -match $IgnoreDomainRegex) {
+                        continue
+                    }
+                }
                 $MastodonUser = [TwitterMastodonReference]::new($User,$Matches,'Name')
                 if (-Not $MastodonUsers.Contains($MastodonUser) -and $MastodonUser.IsValidDomain) {
                     $MastodonUsers.Add($MastodonUser)
                 }
             }
             if ($User.Description -match  $MastodonUserAccountRegex) {
+                if ($IgnoreDomainRegex) {
+                    if ($Matches['MastodonInstance'] -match $IgnoreDomainRegex) {
+                        continue
+                    }
+                }
                 $MastodonUser = [TwitterMastodonReference]::new($User,$Matches,'Description')
                 if (-Not $MastodonUsers.Contains($MastodonUser) -and $MastodonUser.IsValidDomain) {
                     $MastodonUsers.Add($MastodonUser)
                 }
             }
             foreach ($Url in ($User.Entities.Where{$_.GetType() -match 'Url'} )) {
-                if ($PSBoundParameters.ContainsKey('IgnoreUrl')) {
-                    if ($Url.ToString() -match $IgnoreDomain) {
-                        continue
-                    }
-                }
                 if ($Url.ToString() -match  $MastodonUserUrlRegex) {
+                    if ($IgnoreDomainRegex) {
+                        if ($Matches['MastodonInstance'] -match $IgnoreDomainRegex) {
+                            continue
+                        }
+                    }
                     $MastodonUser = [TwitterMastodonReference]::new($User,$Matches,'UrlEntity')
                     if (-Not $MastodonUsers.Contains($MastodonUser) -and $MastodonUser.IsValidDomain) {
                         $MastodonUsers.Add($MastodonUser)
@@ -55,18 +71,23 @@ function Find-TwitterMastodonLinks {
         foreach ($Tweet in $Tweets) {
             $User = $TwitterUsers | Where-Object Id -eq $Tweet.AuthorId | Select-Object -First 1
             if ($Tweet.Text -match $MastodonUserAccountRegex) {
+                if ($IgnoreDomainRegex) {
+                    if ($Matches['MastodonInstance'] -match $IgnoreDomainRegex) {
+                        continue
+                    }
+                }
                 $MastodonUser = [TwitterMastodonReference]::new($User,$Matches,'TweetText')
                 if (-Not $MastodonUsers.Contains($MastodonUser) -and $MastodonUser.IsValidDomain) {
                     $MastodonUsers.Add($MastodonUser)
                 }
             }
             foreach ($Url in ($Tweet.Entities.Where{$_.GetType() -match 'Url'})) {
-                if ($PSBoundParameters.ContainsKey('IgnoreUrl')) {
-                    if ($Url.ToString() -match $IgnoreDomain) {
-                        continue
-                    }
-                }
                 if ($Url.ToString() -match  $MastodonUserUrlRegex) {
+                    if ($IgnoreDomainRegex) {
+                        if ($Matches['MastodonInstance'] -match $IgnoreDomainRegex) {
+                            continue
+                        }
+                    }
                     $MastodonUser = [TwitterMastodonReference]::new($User,$Matches,'TweetUrlEntity')
                     if (-Not $MastodonUsers.Contains($MastodonUser) -and $MastodonUser.IsValidDomain) {
                         $MastodonUsers.Add($MastodonUser)
